a0b85390cefc9d08bba33b8cdc76838a1f348e28
Evan Hoyle <you@example.com>
2025-06-15 14:25:41 -0400

    website functional - testing cookies

---
+++ README.md
@@ -0,0 +1,6 @@
+https://startbootstrap.com/theme/resume
+
+https://startbootstrap.com/template/scrolling-nav
+
+
+https://stackoverflow.com/questions/20723236/displaying-a-welcome-message-if-a-cookie-is-set
---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+/*!
+* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+var shown = false;
+function showhideEmail(){
+  if (shown){
+    document.getElementById('email').innerHTML = "Show my email";
+    shown = false;
+  } else {
+    var myemail = "<a href='mailto:Hoyleed@mail.uc.edu" + "@" + "outlook.com'>Hoyleed" + "@" + "outlook.com</a>";
+    document.getElementById('email').innerHTML = myemail;
+    shown = true;
+  }
+}
